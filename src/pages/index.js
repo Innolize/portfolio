f55e69d98d6f764b5db5c87d2c73c52c6db7d058
Roman <Roman.Mettler@gmail.com>
2020-09-08 23:10:17 -0300

    primer commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div >
+    <div style={{height:120}}>asdasd</div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>Hello asasdasdsadasd!</div>
+    <div>Hello asasdasdsadasd!</div>
+  </div>
 }
