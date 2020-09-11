bd5c0291c806d77fa00cee2e4f275462b58f8c65
Roman <Roman.Mettler@gmail.com>
2020-09-10 23:01:13 -0300

    footer agregado y form para contacto funcional

--- src/componentes/Contacto.jsx
+++ src/componentes/Contacto.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 const Separador = styled.div`
     position: absolute;
@@ -23,6 +25,7 @@ const ContenedorContacto = styled.div`
     display:flex;
     align-items: center;
     flex-direction: column;
+    color:white;
 `
 
 const Contenido = styled.div`
@@ -46,10 +49,12 @@ const Contacto = () => {
             <Contenido>
                 <h1>Contacto</h1>
                 <h3>Tenes alguna pregunta o queres contactarte conmigo?</h3>
-
-                <InputChico placeholder="Nombre"></InputChico>
-                <InputChico placeholder="Email"></InputChico>
-                <InputGrande rows="10" placeholder="Tu mensaje"></InputGrande>
+                <form action="https://getform.io/f/105fb10e-ffa1-496e-b670-f66d7b7b047d" method="POST">
+                    <InputChico name="name" placeholder="Nombre"></InputChico>
+                    <InputChico name="email" placeholder="Email"></InputChico>
+                    <InputGrande name="message" rows="10" placeholder="Tu mensaje"></InputGrande>
+                    <button type="submit">Enviar</button>
+                </form>
             </Contenido>
             <Footer />
         </ContenedorContacto>
@@ -57,14 +62,35 @@ const Contacto = () => {
 }
 
 const ContenedorFooter = styled.div`
+    position: absolute;
+    bottom: 0;
     height:120px;
     width: 100%;
     background-color: black;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+`
+const IconoEstilado = styled(FontAwesomeIcon)`
+    font-size:60px;
+    padding:25px 20px 0px 20px;
+`
+const ContenedorIconos = styled.div`
+    display:flex;
+`
+const FinalDePagina = styled.h5`
+    padding-top: 15px;
+    margin: 0;
 `
 
 const Footer = () => {
     return (
         <ContenedorFooter>
+            <ContenedorIconos>
+                <IconoEstilado icon={faLinkedin} />
+                <IconoEstilado icon={faGithub} />
+            </ContenedorIconos>
+            <FinalDePagina>Román Mettler ©2020</FinalDePagina>
         </ContenedorFooter>
     )
 }
